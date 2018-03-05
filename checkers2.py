23a1972ffe1c104a19e57055dc17491a41a3528e
msinvisible <36958846+msinvisible@users.noreply.github.com>
2018-03-05 22:10:39 +0300

    теперь верно

--- checkers2.py
+++ checkers2.py
@@ -2,10 +2,7 @@ x1 = int(input())
 y1 = int(input())
 x2 = int(input())
 y2 = int(input())
-if x1 != x2 and y1 != y2 and y2 <= 8 and x2 <= 8 and y2 > y1:
-    if (x2-x1+y2-y1) % 2 == 0 and y2-y1 >= abs(x2-x1):
-        print("YES")
-    else:
-        print('NO')
+if (x2 - x1 + y2 - y1) % 2 == 0 and y2 - y1 >= abs(x2 - x1):
+    print("YES")
 else:
     print("NO")
